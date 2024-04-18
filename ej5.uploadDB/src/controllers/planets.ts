d4c92a34451f8cda1e7bc9a1ada301daa61e05b2
Monica Guerra <monik_flute@hotmail.com>
2024-04-18 14:28:40 +0200

    ej5 uploads

--- ej5.uploadDB/src/controllers/planets.ts
+++ ej5.uploadDB/src/controllers/planets.ts
@@ -5,13 +5,13 @@ import pgPromise from "pg-promise";
 const db = pgPromise()("postgres://postgres:postgres@localhost:5432/postgres");
 
 const setupDb = async () => {
-
   await db.none(`
   DROP TABLE IF EXISTS planets;
 
   CREATE TABLE planets (
     id SERIAL NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    image TEXT
   );
   `);
   await db.none(`INSERT INTO PLANETS (name) VALUES ('Earth')`);
@@ -19,7 +19,6 @@ const setupDb = async () => {
   await db.none(`INSERT INTO PLANETS (name) VALUES ('Venus')`);
 
   const planets = await db.many(`SELECT * FROM planets`);
-  console.log(planets);
 };
 setupDb();
 
@@ -31,8 +30,7 @@ const getAll = async (req: Request, res: Response) => {
 const getOneById = async (req: Request, res: Response) => {
   const { id } = req.params;
   const planet = await db.oneOrNone(
-    `
-    SELECT * FROM planets WHERE id=$1`,
+    `SELECT * FROM planets WHERE id=$1`,
     Number(id)
   );
   res.status(200).json(planet);
@@ -51,8 +49,7 @@ const createPost = async (req: Request, res: Response) => {
       .status(400)
       .json({ message: validatedNewPlanet.error.details[0].message });
   } else {
-
-    await db.none(`INSERT INTO planets (name) VALUES ($1)`, name )
+    await db.none(`INSERT INTO planets (name) VALUES ($1)`, name);
     return res
       .status(200)
       .json({ message: "the planet was created and passed the validation" });
@@ -62,14 +59,27 @@ const createPost = async (req: Request, res: Response) => {
 const updateById = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { name } = req.body;
-  await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name] );
+  await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name]);
   res.status(200).json({ message: "planet updated" });
 };
 
 const deleteById = async (req: Request, res: Response) => {
   const { id } = req.params;
- await db.none(`DELETE FROM planets WHERE id=$1`, Number(id));
+  await db.none(`DELETE FROM planets WHERE id=$1`, Number(id));
   res.status(200).json({ message: "planet deleted" });
 };
 
-export { getAll, getOneById, createPost, updateById, deleteById };
+const createImage = async (req: Request, res: Response) => {
+  console.log(req.file);
+  const { id } = req.params;
+  const filename = req.file?.path;
+
+  if(filename){
+    db.none(`UPDATE planets SET image=$2 WHERE id=$1`,[id, filename])
+    res.status(201).json({ message: "planet image uploaded succesfully" });
+  }else{
+    res.status(400).json({ message: "planet failed to upload.." });
+  }
+};
+
+export { getAll, getOneById, createPost, updateById, deleteById, createImage };
---
+++ ej5.uploadDB/src/server.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import "express-async-errors";
+import morgan from "morgan";
+import {
+  getAll,
+  getOneById,
+  createPost,
+  updateById,
+  deleteById,
+  createImage,
+} from "./controllers/planets.js";
+import multer from "multer";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const storage = multer.diskStorage({
+destination: (req,file,cb)=>{
+  cb(null, "./uploads")
+},
+filename: (req,file,cb)=>{
+  cb(null, file.originalname)
+},
+})
+const upload = multer({storage})
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
+
+
+app.get("/api/planets", getAll);
+app.get("/api/planets/:id", getOneById);
+app.post("/api/planets", createPost);
+app.put("/api/planets/:id", updateById);
+app.delete("/api/planets/:id", deleteById);
+app.post("/api/planets/:id/image", upload.single("image"), createImage);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port http://localhost:${PORT}`);
+});
